e22a59f8a4fe8f6ad8325b30de1d8041186a166b
JhoJho19 <eugb46@gmail.com>
2024-07-01 15:32:42 +0300

    01.07.2024 15.32

--- Assets/Scripts/Enviroment/EnemyDestroyer.cs
+++ Assets/Scripts/Enviroment/EnemyDestroyer.cs
@@ -1,6 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyDestroyer : MonoBehaviour
@@ -9,14 +7,19 @@ public class EnemyDestroyer : MonoBehaviour
     [SerializeField] Collider2D colliderOfObjectForDestroy;
     [SerializeField] Collider2D triggerOfObjectForDestroy;
     [SerializeField] Collider2D detectorCollider;
+    [SerializeField] bool isFlying;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            detectorCollider.isTrigger = true;
-            triggerOfObjectForDestroy.enabled = false;
-            colliderOfObjectForDestroy.enabled = false;
+            if (!isFlying) 
+            {
+                detectorCollider.isTrigger = true;
+                triggerOfObjectForDestroy.enabled = false;
+                colliderOfObjectForDestroy.enabled = false;
+            }
+
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 16.5f), ForceMode2D.Impulse);
             StartCoroutine(AnimationAndDestroy());
         }
@@ -24,6 +27,10 @@ public class EnemyDestroyer : MonoBehaviour
 
     IEnumerator AnimationAndDestroy()
     {
+        if (isFlying) 
+        {
+            Destroy(objectForDestroy);
+        }
         yield return new WaitForSeconds(2f);
         Destroy(objectForDestroy);
     }
